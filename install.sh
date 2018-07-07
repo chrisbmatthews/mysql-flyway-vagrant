7a7dcfa569ea091478cadc23ec1954dafb8c6428
Chris Matthews <chris.b.matthews@gmail.com>
2018-07-07 07:49:30 -0300

    fixed bind address for mysql on xenial

--- install.sh
+++ install.sh
@@ -10,7 +10,11 @@ debconf-set-selections <<< 'mysql-server mysql-server/root_password_again passwo
 apt-get install -y vim curl python-software-properties
 apt-get update
 apt-get -y install mysql-server
+#allow connections form outside the vm, so comment out the bind-address in the appropriate file, depending on which version of mysql we have...
+#this is for older versions...
 sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
+#this is for newer versions...
+sed -i "s/^bind-address/#bind-address/" /etc/mysql/mysql.conf.d/mysqld.cnf
 mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES; SET GLOBAL max_connect_errors=10000;"
 /etc/init.d/mysql restart
 
